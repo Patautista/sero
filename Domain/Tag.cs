9230220dce64159b47342de6927c2f5f12ee6cf8
Patautista <caleb.kart@gmail.com>
2025-08-23 12:17:30 -0300

    feat: modelled sentence and tag classes

---
+++ Domain/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Tag
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+    public static class TagTypes
+    {
+        public static string Difficulty { get; set; }
+        public static string LearningTopic { get; set; }
+        public static string GeneralTopic { get; set; }
+    }
+}
---
+++ Domain/ValueObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+                return false;
+
+            var other = (ValueObject)obj;
+
+            return GetEqualityComponents()
+                .SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+            => Equals(left, right);
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+            => !Equals(left, right);
+    }
+}
