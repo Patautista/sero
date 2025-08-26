484fc1ce45eb8c90a40e38a3e7c8e043216b2c80
Patautista <caleb.kart@gmail.com>
2025-08-26 08:32:11 -0300

    feat: language pair + DifficultyLevel

---
+++ Application/DifficultyLevel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public enum DifficultyLevel
+    {
+        Beginner,
+        Intermediate,
+        Advanced
+    }
+
+    public static class DifficultyLevelExtensions
+    {
+        public static DifficultyLevel FromString(string value)
+        {
+            return value?.ToLowerInvariant() switch
+            {
+                "beginner" => DifficultyLevel.Beginner,
+                "intermediate" => DifficultyLevel.Intermediate,
+                "advanced" => DifficultyLevel.Advanced,
+                _ => throw new ArgumentException($"Invalid difficulty: {value}")
+            };
+        }
+    }
+}
---
+++ Application/LanguagePair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class LanguagePair
+    {
+        public CultureInfo Source { get; set; }
+        public CultureInfo Target { get; set; }
+
+        public string DisplayName => $"{Source.NativeName} → {Target.NativeName}";
+    }
+}
