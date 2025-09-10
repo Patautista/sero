b4aa6e06cb920a4d3ef36fe7e99f5642c4fd1253
Patautista <caleb.kart@gmail.com>
2025-09-10 18:39:08 -0300

    feat: mediator support

---
+++ Application/CoreDI.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public static class CoreDI
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(IMediator).Assembly);
+            });
+            return services;
+        }
+    }
+}
