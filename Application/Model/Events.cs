95dfcef19b5b54af163b37e7694ce4f6b2316847
Patautista <caleb.kart@gmail.com>
2025-09-10 23:47:32 -0300

    feat: avg time in seconds

---
+++ Application/Model/Events.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Model
+{
+    public class CardAnsweredDto
+    {
+        public DateTime DateTime { get; set; }
+        public int CardId { get; set; }
+        public int EllapsedMs { get; set; }
+        public int AnswerAttempt { get; set; }
+    }
+
+    public class CardSkippedDto
+    {
+        public DateTime DateTime { get; set; }
+        public int CardId { get; set; }
+        public int AnswerAttempt { get; set; }
+    }
+}
