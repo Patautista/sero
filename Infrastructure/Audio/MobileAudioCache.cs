a1332c507120da6de96582cb655a6c34387402ae
Patautista <caleb.kart@gmail.com>
2025-09-16 18:52:22 -0300

    feat: audio play

---
+++ Infrastructure/Audio/MobileAudioCache.cs
@@ -0,0 +1,48 @@
+﻿using Business.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Audio
+{
+    // Implementação usando arquivos locais
+    public class MobileAudioCache : IAudioCache
+    {
+        private readonly string _basePath;
+
+        public MobileAudioCache(string basePath = "voice_cache")
+        {
+            _basePath = basePath;
+        }
+
+        public async Task<byte[]?> GetAsync(string key)
+        {
+            var path = GetPath(key);
+            if (File.Exists(path))
+            {
+                return await File.ReadAllBytesAsync(path);
+            }
+            return null;
+        }
+
+        public async Task SetAsync(string key, byte[] data)
+        {
+            var path = GetPath(key);
+            await File.WriteAllBytesAsync(path, data);
+        }
+
+        public string ComputeCacheKey(string text, string lang, VoiceGender voiceGender)
+        {
+            using var sha = SHA256.Create();
+            var hash = Convert.ToHexString(
+                sha.ComputeHash(Encoding.UTF8.GetBytes($"{lang}:{voiceGender}:{text}"))
+            );
+            return hash;
+        }
+
+        private string GetPath(string key) => Path.Combine(_basePath, $"{key}.mp3");
+    }
+}
--- MauiApp1/Services/SoundService.cs
+++ MauiApp1/Services/SoundService.cs
@@ -1,17 +1,35 @@
-﻿using Plugin.Maui.Audio;
+﻿using Business.Audio;
+using Microsoft.Maui.Storage;
+using Plugin.Maui.Audio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Maui.Storage;
 
 namespace MauiApp1.Services
 {
-    public static class SoundService
+    public class SoundService
     {
         private static IAudioManager _audioManager = new AudioManager();
-
+        private readonly IAudioCache _audioCache;
+        public SoundService(IAudioCache audioCache)
+        {
+            _audioCache = audioCache;
+        }
+        public async Task PlayVoice(string text, string lang, VoiceGender voiceGender = VoiceGender.Female)
+        {
+            try
+            {
+                var key = _audioCache.ComputeCacheKey(text, lang, voiceGender);
+                var player = _audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync($"Sound/voice_cache/{key}.mp3"));
+                player.Play();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         public static async Task PlaySuccessSound()
         {
             // success.wav must be in Resources/Raw/
