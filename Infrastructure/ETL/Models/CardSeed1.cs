520aab714ad5f28dfd8fedd49de3243f2316f77b
Patautista <caleb.kart@gmail.com>
2025-09-16 16:28:32 -0300

    feat: audio pipeline + cache

--- Infrastructure/ETL/Models/CardSeed1.cs
+++ Infrastructure/ETL/Models/CardSeed1.cs
@@ -15,14 +15,6 @@ namespace Infrastructure.ETL.Models
         public ICollection<Tag> Tags { get; set; }
         public DifficultyLevel DifficultyLevel { get; set; }
 
-        public bool HasTag(string name)
-        {
-            return Tags.Any(t => t.Name == name.ToLower());
-        }
-        public bool SuitsDifficulty(DifficultyLevel difficultyLevel)
-        {
-            return DifficultyLevel <= difficultyLevel;
-        }
         public Card ToDomain()
         {
             return new Card
@@ -32,6 +24,34 @@ namespace Infrastructure.ETL.Models
                 SentencesInTargetLanguage = new List<Sentence> { TargetSentence },
                 Tags = Tags
             };
+        }
+        public record CardAudiov1
+        {
+            public CardAudiov1(Card card, string audioData)
+            {
+                NativeSentence = card.NativeSample;
+                TargetSentence = card.TargetSample;
+                Tags = card.Tags;
+                DifficultyLevel = card.DifficultyLevel;
+                AudioData = audioData;
+            }
+            public Sentence NativeSentence { get; set; }
+            public Sentence TargetSentence { get; set; }
+
+            public string AudioData { get; set; }
+            public ICollection<Tag> Tags { get; set; }
+            public DifficultyLevel DifficultyLevel { get; set; }
+
+            public Card ToDomain()
+            {
+                return new Card
+                {
+                    DifficultyLevel = DifficultyLevel,
+                    SentencesInNativeLanguage = new List<Sentence> { NativeSentence },
+                    SentencesInTargetLanguage = new List<Sentence> { TargetSentence },
+                    Tags = Tags
+                };
+            }
         }
     }
 }
