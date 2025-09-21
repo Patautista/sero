23a825335f6fcdc58b78926ee80286c0b69838a1
Patautista <caleb.kart@gmail.com>
2025-09-21 09:22:49 -0300

    feat: injecting language into other components

---
+++ Infrastructure/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+            return $"{char.ToUpper(input[0])}{input[1..]}";
+        }
+    }
+}
