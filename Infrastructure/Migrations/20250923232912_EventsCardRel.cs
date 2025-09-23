7812e0619917eca706a4a6d31e2303c9a458534a
Patautista <caleb.kart@gmail.com>
2025-09-23 20:51:37 -0300

    fix: remaining refs

---
+++ Infrastructure/Migrations/20250923232912_EventsCardRel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventsCardRel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Cards_CardTableId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CardTableId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "CardTableId",
+                table: "Events");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CardId",
+                table: "Events",
+                column: "CardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Cards_CardId",
+                table: "Events",
+                column: "CardId",
+                principalTable: "Cards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Cards_CardId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CardId",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CardTableId",
+                table: "Events",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CardTableId",
+                table: "Events",
+                column: "CardTableId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Cards_CardTableId",
+                table: "Events",
+                column: "CardTableId",
+                principalTable: "Cards",
+                principalColumn: "Id");
+        }
+    }
+}
