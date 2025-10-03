e16d76307084da038ada1c57b0b0f17cb014519b
Patautista <caleb.kart@gmail.com>
2025-10-03 16:15:34 -0300

    ping controller

---
+++ SupportServer/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SupportServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<bool> GetPing()
+        {
+            return true;
+        }
+    }
+}
