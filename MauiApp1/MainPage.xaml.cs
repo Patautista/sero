ebaabf8e59d7b965345c7d9113c617dc88a09185
Patautista <caleb.kart@gmail.com>
2025-10-12 18:44:02 -0300

    feat: share intent

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -5,6 +5,12 @@
         public MainPage()
         {
             InitializeComponent();
+
+            MessagingCenter.Subscribe<object, string>(this, "SharedText", (sender, text) =>
+            {
+                // Aqui você já cria o Card com o texto
+                DisplayAlert("Novo Card", $"Texto recebido: {text}", "OK");
+            });
         }
     }
 }
