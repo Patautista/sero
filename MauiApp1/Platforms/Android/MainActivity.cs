0f0ff011bd5b191d9dd629000f26706631f4f20b
Patautista <caleb.kart@gmail.com>
2025-10-12 18:44:02 -0300

    feat: share intent

--- MauiApp1/Platforms/Android/MainActivity.cs
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -1,11 +1,46 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
 namespace MauiApp1
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(
+    new[] { Intent.ActionSend },
+    Categories = new[] { Intent.CategoryDefault },
+    DataMimeType = "text/plain")]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            HandleIntent(Intent);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            Intent = intent; // Important: update the Intent property
+            HandleIntent(intent);
+        }
+
+        private void HandleIntent(Intent? intent)
+        {
+            if (intent?.Action == Intent.ActionSend && intent.Type == "text/plain")
+            {
+                var sharedText = intent.GetStringExtra(Intent.ExtraText);
+                if (!string.IsNullOrEmpty(sharedText))
+                {
+                    // Manda esse texto pro MAUI
+                    MessagingCenter.Send<object, string>(this, "SharedText", sharedText);
+                }
+            }
+        }
     }
 }
